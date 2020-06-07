043558b9065e0009ea2ce3391d44a76f60beefe2
Bijan Fazeli <bfazeli1@gmail.com>
2020-06-07 14:03:00 -0700

    Initial commit

---
+++ app/src/main/java/com/connect_worlds/giffitup/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.connect_worlds.giffitup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
